b01a307cfc19765720a662d4fb300487aa73a6a2
Anton Paskanny <anton.paskanny@gmail.com>
2018-12-20 15:48:10 +0300

    01 - Add Drum Kit

---
+++ 01 - JavaScript Drum Kit/index.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', e => {
+    // Internal variabels
+    const KEYS = document.querySelectorAll(".key");
+    const TRANSITION_CLASS = 'playing';
+    
+
+    // Event listeners
+    /**
+     * Window event listener to detect which key was pressed
+     * @param {object} e - event object
+     */
+    function onKeyDown(e) {
+        const keyCode = e.keyCode;
+        const queryAudioStr = `audio[data-key="${keyCode}"]`;
+        const queryKeyStr = `.keys div[data-key="${keyCode}"]`;
+
+        const audio = document.querySelector(queryAudioStr);
+        const keyElem = document.querySelector(queryKeyStr);
+
+        if (!audio) return;
+
+        // Reset time to allow sounding on multiple keypress
+        audio.currentTime = 0;
+        audio.play();
+
+        keyElem.classList.add(TRANSITION_CLASS);
+    }
+
+    /**
+     * Transition end listener for UI keys to remove transition's class and set styles to default
+     * @param e - event object
+     */
+    function onTransitionEnd(e) {
+        // Transition is set to 'all' in styles. We don't need to invoke 'remove' method on each property transition end.
+        if (e.propertyName !== 'transform') return;
+        this.classList.remove(TRANSITION_CLASS)
+    }
+
+
+    //Initialization
+    KEYS.forEach(key => key.addEventListener('transitionend', onTransitionEnd));
+    window.addEventListener('keydown', onKeyDown);
+});
